d5373adf31bcd33cb2c845765ac7028693889158
Chegenelius <55439401+Chegenelius@users.noreply.github.com>
2022-05-18 18:51:43 +0300

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,15 @@
+package ke.co.icomfy;
+
+public class Area{
+    public static void main(String[] args){
+double radius;
+double area;
+
+radius=7;
+area=radius * radius * 3.14;
+
+System.out.println("the area of the circle of radius" + radius + "is"+ area);
+    }
+
+    
+}
