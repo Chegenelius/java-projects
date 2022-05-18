0c24cae12e94b5e23ddcbf344121daeab9e7b422
Chegenelius <55439401+Chegenelius@users.noreply.github.com>
2022-05-18 18:53:12 +0300

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,17 @@
+package ke.co.icomfy;
+
+public class Loop{
+    
+    public (int[]marks){
+        int sum=0;
+
+        for(int mark : marks){
+            sum += mark;
+        }
+        return sum;
+    }
+    }
+
+    
+    
+   
